1a16ace6ab380aaaa9fecb4c527372fac54d659a
finless88 <yanankhan46@gmail.com>
2023-03-02 22:35:15 +0600

    dynamically added card with card id in card container\

--- universe.js
+++ universe.js
@@ -6,5 +6,28 @@ const loadData = () => {
     .then((data) => displayData(data.data));
 };
 const displayData = (data) => {
-  console.log(data.tools);
+  //console.log(data.tools);
+
+  //catch card container
+  const cardContainer = document.getElementById("cardContainer");
+  data.tools.forEach((element) => {
+    console.log(element);
+    const newCard = document.createElement("div");
+    newCard.classList.add("col");
+    newCard.innerHTML = `
+    <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+            <h5>id: ${element.id}</h5>
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+    `;
+    cardContainer.appendChild(newCard);
+  });
 };
