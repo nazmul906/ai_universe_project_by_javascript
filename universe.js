aaca844bcfad53b7a5a6cf08fb61c9f20696d7d2
finless88 <yanankhan46@gmail.com>
2023-03-02 22:17:11 +0600

    fetched api  and displayed data

--- universe.js
+++ universe.js
@@ -1,1 +1,10 @@
 console.log("universe");
+const url = `https://openapi.programming-hero.com/api/ai/tools`;
+const loadData = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayData(data.data));
+};
+const displayData = (data) => {
+  console.log(data.tools);
+};
