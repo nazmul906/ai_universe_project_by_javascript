4bd823bb9f5b39d7e1433f597a47ba02b47dfcdf
finless88 <yanankhan46@gmail.com>
2023-03-03 01:26:53 +0600

    bootstrap spinner and it's functionality is added

--- universe.js
+++ universe.js
@@ -1,17 +1,36 @@
 console.log("universe");
 const url = `https://openapi.programming-hero.com/api/ai/tools`;
 const loadData = () => {
+  toggleLoader(true); //starting the spinner
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayData(data.data));
+    .then((data) => {
+      displayData(data.data);
+    });
 };
+
+// const loadData = async () => {
+//   //   console.log(searchtext);
+//   const url = `https://openapi.programming-hero.com/api/ai/tools`;
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   displayData(data.data);
+//   //   console.log(data.data);
+// };
 const displayData = (data) => {
-  console.log(data.tools.slice(0, 6));
+  //   console.log(data.tools.slice(0, 6));
+  //   console.log(data.tools.length);
+
+  if (data.tools.length > 6) {
+    data = data.tools.slice(0, 6);
+    const showmore = document.getElementById("showMore");
+    showmore.classList.remove("d-none");
+  }
 
   //catch card container
   const cardContainer = document.getElementById("cardContainer");
-  data.tools.slice(0, 6).forEach((element) => {
-    console.log(element);
+  data.forEach((element) => {
+    // console.log(element);
 
     const newCard = document.createElement("div");
     newCard.classList.add("col");
@@ -37,4 +56,16 @@ const displayData = (data) => {
     `;
     cardContainer.appendChild(newCard);
   });
+  //   stoping the spinner
+  toggleLoader(false);
+};
+const toggleLoader = (isloading) => {
+  const spinLoader = document.getElementById("spinLoader");
+  if (isloading) {
+    spinLoader.classList.remove("d-none");
+    console.log("loading");
+  } else {
+    spinLoader.classList.add("d-none");
+    console.log("loaded");
+  }
 };
